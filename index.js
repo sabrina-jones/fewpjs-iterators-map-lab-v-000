aeaa1eea5913ac79c90b65f521f06a93f10f2680
Sabrina Jones <sabrina.jones87@gmail.com>
2021-03-11 02:59:54 +0000

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,16 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial => {
+    let sentence = tutorial.split(" ");
+    for(let i = 0; i < sentence.length; i++){
+      sentence[i] = sentence[i][0].toUpperCase() + sentence[i].slice(1);
+    }
+    return sentence.join(" ");
+    let words = tutorial.split(" ");
+    let titleCasedSentence =
+      words.map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    let response = titleCasedSentence.join(" ");
+    return response
+  })
 }
